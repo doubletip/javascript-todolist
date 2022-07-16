be21cfdd7f60b3419a4f4917b24c9a73104277de
이지원 <double_tip@naver.com>
2022-07-16 23:13:38 +0900

    리스트 추가 수정

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ const TodoButton = document.querySelector('.button')
 const TodoUl = document.querySelector('.todoUl')
 
 TodoButton.addEventListener('click', ()=> {
+    // if(TodoInput.value.length > 0) {
+    //     const TodoLi = document.createElement('li')
+    //     TodoLi.innerHTML = TodoInput.value
+        
+
+    //     TodoUl.appendChild(TodoLi)
+    //     TodoInput.value = ""
+    // } else {
+    //     alert("할 일을 입력하세요.")
+    // }
+
     if(TodoInput.value.length > 0) {
         const TodoLi = document.createElement('li')
-        TodoLi.innerHTML = TodoInput.value
+        TodoLi.classList.add('todoLi')
+        // TodoLi.innerHTML = TodoInput.value
+        TodoLi.innerHTML = `
+        ${TodoInput.value}
+        <div class="btn_wrap">
+                <button class="del">삭제</button>
+                <button class="edit">수정</button>
+            </div>
+        `
+        
+
         TodoUl.appendChild(TodoLi)
         TodoInput.value = ""
     } else {
